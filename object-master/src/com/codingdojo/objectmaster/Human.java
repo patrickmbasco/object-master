2ee5ae2df5c1f82609f6ed33fef4a836bd391f46
patrickmbasco <59862023+patrickmbasco@users.noreply.github.com>
2020-04-13 19:55:09 -0700

    Add files via upload

---
+++ object-master/src/com/codingdojo/objectmaster/Human.java
@@ -0,0 +1,52 @@
+package com.codingdojo.objectmaster;
+
+public class Human {
+	private int strength;
+	private int health;
+	private int intelligence;
+	private int stealth;
+	
+	public Human() {
+		this.strength = 3;
+		this.intelligence = 3;
+		this.stealth = 3;
+		this.health = 100;
+	}
+	
+	public void attack(Human someone) {
+		someone.setHealth(someone.getHealth() - this.getStrength());
+	}
+	
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	
+	public int getIntelligence() {
+		return intelligence;
+	}
+
+	public void setIntelligence(int intelligence) {
+		this.intelligence = intelligence;
+	}
+	
+	public int getStealth() {
+		return stealth;
+	}
+
+	public void setStealth(int stealth) {
+		this.stealth = stealth;
+	}
+	
+	public int getHealth() {
+		System.out.println(String.format("Current health: %d.", health));
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+}
---
+++ object-master/src/com/codingdojo/objectmaster/HumanTest.java
@@ -0,0 +1,10 @@
+package com.codingdojo.objectmaster;
+
+public class HumanTest {
+	public static void main(String[] args) {
+		Human player = new Human();
+		Human someone = new Human();
+		player.attack(someone);
+		System.out.println(someone.getHealth());;
+	}
+}
